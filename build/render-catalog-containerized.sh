113ae622bf3a082b6ae3559b55c62d5cdfcffac1
Daniel Farrell <dfarrell@redhat.com>
2025-07-27 13:57:44 -0400

    Make render catalog script loop
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- build/render-catalog-containerized.sh
+++ build/render-catalog-containerized.sh
@@ -2,18 +2,19 @@
 set -ex
 
 OPM_IMAGE="quay.io/operator-framework/opm:latest"
-CATALOG_TEMPLATE="catalog-template-4-17.yaml"
-OUTPUT_CATALOG="catalog-4-17.yaml"
 
-echo "Rendering ${CATALOG_TEMPLATE} to ${OUTPUT_CATALOG} using a container..."
+# Find all catalog templates, excluding older versions
+catalog_templates=$(find . -name "catalog-template-*.yaml" | grep -v -e "4-14" -e "4-15" -e "4-16")
 
-# This command runs 'opm' in a container to render the catalog template.
-# It mounts the current directory to /work in the container, so opm can access the template file.
-# It also mounts the host's container policy to ensure images can be pulled.
-# The output of the command is redirected to create the final catalog file.
-podman run --rm -v "$(pwd)":/work:z -v /etc/containers:/etc/containers:ro -w /work "${OPM_IMAGE}" \
-  alpha render-template basic "${CATALOG_TEMPLATE}" \
-  -o=yaml --migrate-level=bundle-object-to-csv-metadata > "${OUTPUT_CATALOG}"
+for catalog_template in ${catalog_templates}; do
+  output_catalog="${catalog_template//-template/}"
+  echo "Rendering ${catalog_template} to ${output_catalog} using a container..."
 
-echo "Rendering complete. Check the ${OUTPUT_CATALOG} file."
+  podman run --rm -v "$(pwd)":/work:z -v /etc/containers:/etc/containers:ro -w /work "${OPM_IMAGE}" \
+    alpha render-template basic "${catalog_template}" \
+    -o=yaml --migrate-level=bundle-object-to-csv-metadata > "${output_catalog}"
 
+  echo "Rendering complete for ${output_catalog}"
+done
+
+echo "All rendering complete."
