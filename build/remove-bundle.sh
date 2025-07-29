e601072e422e5aaea5699b96f3a93800b9781511
Daniel Farrell <dfarrell@redhat.com>
2025-07-29 10:02:05 -0400

    Add bundle removal script and test
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

---
+++ build/remove-bundle.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+catalog_template_path=${1}
+bundle_version=${2}
+
+if [[ -z "${catalog_template_path}" || -z "${bundle_version}" ]]; then
+  echo "error: Missing arguments. Usage: remove-bundle.sh <catalog_template_path> <bundle_version>"
+  exit 1
+fi
+
+bundle_name=$(yq e '.entries[] | select(.schema == "olm.bundle") | select(.name | test(".*'"${bundle_version}"'.*")) | .name' "${catalog_template_path}")
+bundle_image=$(yq e '.entries[] | select(.schema == "olm.bundle") | select(.name | test(".*'"${bundle_version}"'.*")) | .image' "${catalog_template_path}")
+
+if [[ -z "${bundle_name}" ]]; then
+  echo "error: Bundle with version ${bundle_version} not found in ${catalog_template_path}"
+  exit 1
+fi
+
+# Remove bundle entry
+yq e 'del(.entries[] | select(.image == "'"${bundle_image}"'"))' -i "${catalog_template_path}"
+
+# Remove bundle from channels
+yq e '(.entries[] | select(.schema == "olm.channel").entries) |= del(.[] | select(.name == "'"${bundle_name}"'"))' -i "${catalog_template_path}"
