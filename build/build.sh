322195e5f07f7ca6ff9c32fa59addf41750e5cd8
Daniel Farrell <dfarrell@redhat.com>
2025-07-29 11:57:45 -0400

    Add dedicated build script
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

---
+++ build/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPO_ROOT_DIR=$(realpath "${SCRIPT_DIR}/..")
+
+# Run from the repo root
+cd "${REPO_ROOT_DIR}"
+
+echo "--> Generating catalog template..."
+./scripts/generate-catalog-template.sh
+
+echo "--> Rendering containerized catalog..."
+./build/render-catalog-containerized.sh
+
+echo "--> Build complete."
