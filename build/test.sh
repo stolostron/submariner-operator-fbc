d1a0f18acc6fc49e7aa13c1d6b08cebb2ccd2851
Daniel Farrell <dfarrell@redhat.com>
2025-07-29 10:55:50 -0400

    Update paths for scripts/ dir in main test script
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- build/test.sh
+++ build/test.sh
@@ -13,14 +13,14 @@ cd "${REPO_ROOT_DIR}"
 
 # Add a bundle
 # Get bundle info
-eval $(./build/get-bundle-metadata.sh DUMMY_BUNDLE_IMAGE)
+eval $(./scripts/get-bundle-metadata.sh DUMMY_BUNDLE_IMAGE)
 
 # Add the bundle to the base catalog template
 #./build/add-bundle-to-template.sh catalog-template.yaml "${BUNDLE_IMAGE}" "submariner-product.${BUNDLE_VERSION}-${BUNDLE_DIGEST:0:7}" "${BUNDLE_VERSION}" "${BUNDLE_CHANNELS}"
 # TODO Add removal script
 
 echo "### Running generate-catalog-template.sh ###"
-./build/generate-catalog-template.sh
+./scripts/generate-catalog-template.sh
 
 echo "### Running render-catalog-containerized.sh ###"
 ./build/render-catalog-containerized.sh
@@ -30,13 +30,13 @@ rm -f catalog-template-4-*.yaml
 
 # Assert no difference in relevant files outside build/
 echo "Checking for changes outside 'build/' directory..."
-if ! git diff --exit-code -- . ':!build/'; then
+if ! git diff --exit-code -- . ':!build/' ':!scripts/' ':!test/'; then
   echo "Error: Changes detected outside the 'build/' directory. Please commit or discard them."
   exit 1
 fi
 
 # Assert no untracked or uncommitted changes outside build/
-if git status --porcelain -- . ':!build/' | grep -q .; then
+if git status --porcelain -- . ':!build/' ':!scripts/' ':!test/' | grep -q .; then
   echo "Error: Untracked or uncommitted changes detected outside the 'build/' directory. Please commit or discard them."
   exit 1
 fi
