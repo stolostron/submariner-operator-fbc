05144440cbd4f198f1457e94fcffc4e349a49973
Daniel Farrell <dfarrell@redhat.com>
2025-07-29 13:51:31 -0400

    Don't expected gen'd files at end of build test
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- test/test-build.sh
+++ test/test-build.sh
@@ -17,14 +17,6 @@ echo "--> Running build script..."
 
 echo "--> Verifying build output..."
 
-# Check that the expected number of catalog-template files were created
-num_templates=$(ls catalog-template-4-*.yaml | wc -l)
-if [[ "${num_templates}" -eq 0 ]]; then
-  echo "Error: No catalog-template files were generated."
-  exit 1
-fi
-echo "  [SUCCESS] Found ${num_templates} catalog-template files."
-
 # Check that the expected number of catalog directories were created
 num_catalogs=$(ls -d catalog-4-*/ | wc -l)
 if [[ "${num_catalogs}" -eq 0 ]]; then
