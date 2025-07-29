d66b91c2b9930531c6c15f0c615ac992525a0d83
Daniel Farrell <dfarrell@redhat.com>
2025-07-29 13:15:23 -0400

    Make test.sh just a meta runner
    Build test is one of the scripts, don't dup.
    
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- test/test.sh
+++ test/test.sh
@@ -8,45 +8,37 @@ REPO_ROOT_DIR=$(realpath "${SCRIPT_DIR}/..")
 # Run from the repo root
 cd "${REPO_ROOT_DIR}"
 
+check_git_status() {
+  echo "--> Verifying git status..."
+  # Assert no difference in relevant files outside build/
+  if ! git diff --exit-code -- . ':!build/' ':!scripts/' ':!test/' ':!.github/'; then
+    echo "Error: Changes detected outside ignored directories after running the previous test."
+    exit 1
+  fi
+
+  # Assert no untracked or uncommitted changes outside build/
+  if git status --porcelain -- . ':!build/' ':!scripts/' ':!test/' ':!.github/' | grep -q .; then
+    echo "Error: Untracked or uncommitted changes detected outside ignored directories after running the previous test."
+    exit 1
+  fi
+  echo "  [SUCCESS] Git status is clean."
+}
+
 # Cleanup any previous runs
 ./build/cleanup-generated-files.sh
 
-# Add a bundle
-# Get bundle info
-eval $(./scripts/get-bundle-metadata.sh DUMMY_BUNDLE_IMAGE)
-
-# Add the bundle to the base catalog template
-#./build/add-bundle-to-template.sh catalog-template.yaml "${BUNDLE_IMAGE}" "submariner-product.${BUNDLE_VERSION}-${BUNDLE_DIGEST:0:7}" "${BUNDLE_VERSION}" "${BUNDLE_CHANNELS}"
-# TODO Add removal script
-
-echo "### Running generate-catalog-template.sh ###"
-./scripts/generate-catalog-template.sh
-
-echo "### Running render-catalog-containerized.sh ###"
-./build/render-catalog-containerized.sh
-
-# Cleanup run
-rm -f catalog-template-4-*.yaml
-
-# Assert no difference in relevant files outside build/
-echo "Checking for changes outside ignored directories..."
-if ! git diff --exit-code -- . ':!build/' ':!scripts/' ':!test/' ':!.github/'; then
-  echo "Error: Changes detected outside ignored directories. Please commit or discard them."
-  exit 1
-fi
-
-# Assert no untracked or uncommitted changes outside build/
-if git status --porcelain -- . ':!build/' ':!scripts/' ':!test/' ':!.github/' | grep -q .; then
-  echo "Error: Untracked or uncommitted changes detected outside ignored directories. Please commit or discard them."
-  exit 1
-fi
-
-echo "### Test complete ###"
+echo "### Running all tests... ###"
 
 for test_script in ./test/test-*.sh; do
+  if [[ "$test_script" == "./test/test.sh" ]]; then
+      continue
+  fi
   echo "### Running $test_script ###"
   "$test_script"
+  check_git_status
 done
+
+echo "### All tests complete ###"
 
 # Cleanup after run
 ./build/cleanup-generated-files.sh
