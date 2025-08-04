704b5adcf4405a8cfc997fabb46c9650beefbd38
Daniel Farrell <dfarrell@redhat.com>
2025-08-04 16:04:38 -0400

    Clean up add-bundle tests
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- test/test-add-bundle.sh
+++ test/test-add-bundle.sh
@@ -35,17 +35,11 @@ EOF
 cp "${TEST_CATALOG_TEMPLATE}" "${ORIGINAL_CATALOG_TEMPLATE}"
 
 # Test data
-TEST_BUNDLE_IMAGE="test-registry/test-bundle:v0.0.1"
-TEST_BUNDLE_VERSION="v0.0.1"
-TEST_BUNDLE_CHANNELS="alpha,beta"
+TEST_BUNDLE_IMAGE="quay.io/redhat-user-workloads/submariner-tenant/submariner-bundle-0-21@sha256:bad7ce0c4a3edcb12dc3adf3e4165bd57631c06bc81ed9b507377b12e73f905c"
 
 # Run the script
-echo "Testing add-bundle-to-template.sh..."
-./scripts/add-bundle-to-template.sh \
-  "${TEST_CATALOG_TEMPLATE}" \
-  "${TEST_BUNDLE_IMAGE}" \
-  "${TEST_BUNDLE_VERSION}" \
-  "${TEST_BUNDLE_CHANNELS}"
+echo "Testing add-bundle.sh..."
+./build/add-bundle.sh "${TEST_BUNDLE_IMAGE}"
 
 # Verification
 
@@ -55,15 +49,8 @@ if ! yq '.entries[] | select(.image == "'""${TEST_BUNDLE_IMAGE}""'")' "${TEST_CA
   exit 1
 fi
 
-# Check if the bundle was added to the alpha channel
-if ! yq '.entries[] | select(.schema == "olm.channel") | select(.name == "alpha") | .entries[] | select(.name == "submariner.'"${TEST_BUNDLE_VERSION}"'")' "${TEST_CATALOG_TEMPLATE}" > /dev/null; then
-  echo "Test failed: Bundle not found in alpha channel entries."
-  exit 1
-fi
-
-# Check if the beta channel was created and bundle added
-if ! yq '.entries[] | select(.schema == "olm.channel") | select(.name == "beta") | .entries[] | select(.name == "submariner.'"${TEST_BUNDLE_VERSION}"'")' "${TEST_CATALOG_TEMPLATE}" > /dev/null; then
-  echo "Test failed: Beta channel not created or bundle not found in beta channel entries."
+if ! yq '.entries[] | select(.schema == "olm.channel") | select(.name == "stable-0.21") | .entries[] | select(.name == "submariner.v0.21.0-rc0")' "${TEST_CATALOG_TEMPLATE}" > /dev/null; then
+  echo "Test failed: Bundle not found in stable-0.21 channel entries."
   exit 1
 fi
 
