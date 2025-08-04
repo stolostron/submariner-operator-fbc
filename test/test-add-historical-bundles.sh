951e99944d6ae691aee7953570841b2802b7c019
Daniel Farrell <dfarrell@redhat.com>
2025-08-03 22:18:45 -0400

    Add test of historic release bundle add/buiilds
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

---
+++ test/test-add-historical-bundles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+./scripts/reset-test-environment.sh
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPO_ROOT_DIR=$(realpath "${SCRIPT_DIR}/..")
+
+# Run from the repo root
+cd "${REPO_ROOT_DIR}"
+
+# Read the historical bundle images from the YAML file
+IMAGES=$(yq e '.historical_bundle_images[].image' "${REPO_ROOT_DIR}/test/historical-bundle-images.yaml")
+
+for IMAGE in ${IMAGES}; do
+  echo "--> Adding bundle: ${IMAGE}"
+  make add-bundle BUNDLE_IMAGE="${IMAGE}"
+  echo "--> Validating catalog after adding bundle: ${IMAGE}"
+  make validate-catalog
+done
+
+echo "--> All historical bundles added."
